ff5a53f8e6e529f6c0f7a389a95a2c3e255a3f7c
Oleksandr Chervonniy <aschervonniy@gmail.com>
2023-01-10 21:02:45 +0100

    transfer HTML layout

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,1 +1,8 @@
-export class DomListener {}
+export class DomListener {
+	constructor($root) {
+		if (!$root) {
+			throw new Error(`No $root provided for DomListener`);
+		}
+		this.$root = $root;
+	}
+}
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,8 +1,8 @@
-import { DomListener } from ".DomListener";
+import { DomListener } from "@core/DomListener";
 
 export class ExcelComponent extends DomListener {
+	// Возвращает шаблон компонента
 	toHTML() {
-		// Возвращает шаблон компонента //
 		return "";
 	}
 }
